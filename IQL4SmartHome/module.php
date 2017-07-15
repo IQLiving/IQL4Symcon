01414b89dd7b6158d67e2670a327e7271231a487
Dave <dave@rnmail.eu>
2017-07-15 19:37:59 +0200

    fix HSVtoRGB

--- IQL4SmartHome/module.php
+++ IQL4SmartHome/module.php
@@ -281,31 +281,31 @@ class IQL4SmartHome extends IPSModule {
 
         $hsvToRGB = function ($iH, $iS, $iV) {
 
-            if($iH < 0)   $iH = 0;
-            if($iH > 360) $iH = 360;
-            if($iS < 0)   $iS = 0;
-            if($iS > 100) $iS = 100;
-            if($iV < 0)   $iV = 0;
-            if($iV > 100) $iV = 100;
-            $dS = $iS/100.0;
-            $dV = $iV/100.0;
-            $dC = $dV*$dS;
-            $dH = $iH/60.0;
-            $dT = $dH;
-            while($dT >= 2.0) $dT -= 2.0;
-            $dX = $dC*(1-abs($dT-1));
-            switch($dH) {
-                case($dH >= 0.0 && $dH < 1.0):
+            if($iH < 0)   $iH = 0;   // Hue:
+            if($iH > 360) $iH = 360; //   0-360
+            if($iS < 0)   $iS = 0;   // Saturation:
+            if($iS > 100) $iS = 100; //   0-100
+            if($iV < 0)   $iV = 0;   // Lightness:
+            if($iV > 100) $iV = 100; //   0-100
+            $dS = $iS/100.0; // Saturation: 0.0-1.0
+            $dV = $iV/100.0; // Lightness:  0.0-1.0
+            $dC = $dV*$dS;   // Chroma:     0.0-1.0
+            $dH = $iH/60.0;  // H-Prime:    0.0-6.0
+            $dT = $dH;       // Temp variable
+            while($dT >= 2.0) $dT -= 2.0; // php modulus does not work with float
+            $dX = $dC*(1-abs($dT-1));     // as used in the Wikipedia link
+            switch(floor($dH)) {
+                case 0:
                     $dR = $dC; $dG = $dX; $dB = 0.0; break;
-                case($dH >= 1.0 && $dH < 2.0):
+                case 1:
                     $dR = $dX; $dG = $dC; $dB = 0.0; break;
-                case($dH >= 2.0 && $dH < 3.0):
+                case 2:
                     $dR = 0.0; $dG = $dC; $dB = $dX; break;
-                case($dH >= 3.0 && $dH < 4.0):
+                case 3:
                     $dR = 0.0; $dG = $dX; $dB = $dC; break;
-                case($dH >= 4.0 && $dH < 5.0):
+                case 4:
                     $dR = $dX; $dG = 0.0; $dB = $dC; break;
-                case($dH >= 5.0 && $dH < 6.0):
+                case 5:
                     $dR = $dC; $dG = 0.0; $dB = $dX; break;
                 default:
                     $dR = 0.0; $dG = 0.0; $dB = 0.0; break;

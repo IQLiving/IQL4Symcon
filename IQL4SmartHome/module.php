73a3a9365a4f65e9e082d9e47e1db382d231f599
Dave <dave@rnmail.eu>
2017-08-25 13:28:58 +0200

    fix SceneLocation

--- IQL4SmartHome/module.php
+++ IQL4SmartHome/module.php
@@ -835,12 +835,12 @@ class IQL4SmartHome extends IPSModule {
                     //We only need to add annotations. Remaining data is merged from persistance automatically.
                     //Order is determinted by the order of array elements
                     if(IPS_ObjectExists($treeRowSc['ID'])) {
-                        $data['elements'][2]['values'][] = Array(
+                        $data['elements'][3]['values'][] = Array(
                             "Script" => IPS_GetLocation($treeRowSc['ID']),
                             "State" => "OK",
                         );
                     } else {
-                        $data['elements'][2]['values'][] = Array(
+                        $data['elements'][3]['values'][] = Array(
                             "Script" => "Not found!",
                             "rowColor" => "#ff0000"
                         );
